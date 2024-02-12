37428a659947b1c9df73aaa7be14b961bca25dbd
hasanalam24 <hasanalam365@gmail.com>
2024-02-13 00:12:07 +0600

    rectriangle add

--- scripts/rectriangle.js
+++ scripts/rectriangle.js
@@ -1,6 +1,22 @@
 // Rectriangle Card Area
 
 function CalculateRectangleArea() {
-    const rectangleBtn = document.getElementById('rectriangle-w');
-    const re
+    //  get length of the rectriangle
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText)
+    console.log(length)
+
+    // get width of the rectriangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText)
+    console.log(width)
+    // calculate rectriangle area
+    const area = length * width;
+    console.log('area of the rectriangle is:', area);
+
+    const rectangleDisplay = document.getElementById('rectangle-display');
+    rectangleDisplay.innerText = area
+    console.log(displayValue)
 }
